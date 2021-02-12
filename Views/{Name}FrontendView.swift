ad886e83f339f65b8cb9008c97171787b2b3d672
Tibor Bödecs <mail.tib@gmail.com>
2021-02-12 12:07:04 +0100

    View improvements

--- Views/{Name}FrontendView.swift
+++ Views/{Name}FrontendView.swift
@@ -9,8 +9,6 @@ import FeatherCore
 
 struct {Name}FrontendView {
 
-    let name = "frontend"
-
     let req: Request
 
     init(_ req: Request) {
@@ -18,19 +16,21 @@ struct {Name}FrontendView {
     }
 
     private func render(_ name: String, _ context: LeafRenderer.Context) -> EventLoopFuture<View> {
-        let template = "\({Name}Module.name.capitalized)/\(self.name.capitalized)/\(name.capitalized)"
+        let template = "\({Name}Module.name.capitalized)/Frontend/\(name)"
         return req.leaf.render(template: template, context: context)
     }
 
     func {name}(_ {name}: [{Name}Model], metadata: Metadata) -> EventLoopFuture<View> {
-        render("{name}", [
-                "metadata": metadata.leafData,
-                "{name}": .array({name}.map { $0.leafDataWithJoinedMetadata }),
+        render("{Name}List", [
+            "metadata": metadata.leafData,
+            "{name}": .array({name}.map { $0.leafDataWithJoinedMetadata }),
         ])
     }
 
     func {name}(_ {name}: {Name}Model) -> EventLoopFuture<View> {
-        render("{name}", ["{name}": {name}.leafData])
+        render("{Name}", [
+            "{name}": {name}.leafData
+        ])
     }
 
 }
